75c366b05a77037a2a2f154916123b6b645bd1aa
PUNITH KUMAR <215137747+elitepunith@users.noreply.github.com>
2026-05-03 16:01:03 +0530

    updated

--- script.js
+++ script.js
@@ -24,3 +24,34 @@ pic2.addEventListener('click', function() {
     sound2.play();
 
   });
+
+  
+  const pic3 = document.getElementById('tim');
+const sound3 = document.getElementById('tim1');
+
+pic3.addEventListener('click', function() {
+
+    sound3.play();
+
+  });
+
+   
+  const pic4 = document.getElementById('yop');
+const sound4 = document.getElementById('yop1');
+
+pic4.addEventListener('click', function() {
+
+    sound4.play();
+
+  });
+
+
+   
+  const pic5 = document.getElementById('kimo');
+const sound5 = document.getElementById('kimo1');
+
+pic5.addEventListener('click', function() {
+
+    sound5.play();
+
+  });
